691e9e5601e03eaee6df97f3c6ff0cdebb0e6dbc
monk-time <monk-time@users.noreply.github.com>
2017-12-11 20:33:40 +0300

    Fix ESLint errors

--- 3rd-party/IMDb - List Helper.user.js
+++ 3rd-party/IMDb - List Helper.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
-// @name           IMDb List Helper
-// @namespace      imdb
+// @name           IMDb - List Helper
 // @description    Makes creating IMDb lists more efficient and convenient
-// @version        2.4.0
+// @namespace      imdb
+// @author         themagician, monk-time
 // @include        http://*imdb.com/list/*/edit
-// @require        https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js
+// @include        http://*imdb.com/list/*/edit?*
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @icon           http://www.imdb.com/favicon.ico
 // @grant          GM_addStyle
+// @version        2.4.0
 // ==/UserScript==
-
-var jQuery = unsafeWindow.jQuery;
-var $ = jQuery;
 
 //
 // CHANGELOG
 //
+// 3.0
+// fixed: enable on pages with a referal in the query string
+// changed: criticker score conversion: 0..10 -> 1, 11..20 -> 2, 91..100 -> 10
+//
 // 2.4.0
-// bugfix: IMDb changed layout
+// fixed: IMDb changed layout
 //
 // 2.3.0
-// bugfix: importing ratings works again
+// fixed: importing ratings works again
 //
 // 2.2.0
 // added: support for people
@@ -40,287 +44,275 @@ var $ = jQuery;
 // fixed: some entries are skipped when adding imdb ids/urls
 //
 
-//
+'use strict';
+
 // milliseconds between each request
-//
-var REQUEST_DELAY = 1000;
+const REQUEST_DELAY = 1000;
 
-function processImdb(file) {
-    var csv_lines = file.split("\n");
-    for(var i = 1; i < csv_lines.length; ++i) {
-        try {
-            var data = jQuery.csv.toArray(csv_lines[i]);
-            var rating = data[8];
-            if(rating === "") {
-                continue;
-            }
-
-            $("#filmList").append(rating + "," + data[1] + "\n");
-        }
-        catch(e) {
-            console.log("Exception: " + e);
-            console.log("Bad line: " + csv_lines[i]);
-        }
-    }
-}
-
-function processRym(file) {
-    var csv_lines = file.split("\n");
-    for(var i = 1; i < csv_lines.length; ++i) {
-        try {
-            var data = jQuery.csv.toArray(csv_lines[i]);
-            if(data.length < 6) {
-                continue;
-            }
-
-            $("#filmList").append(data[4] + "," + data[1] + "\n");
-        }
-        catch(e) {
-            console.log("Exception: " + e);
-            console.log("Bad line: " + csv_lines[i]);
-        }
-    }
-}
-
-function processCriticker(file) {
-    var lines = file.split("\n");
-    for(var i = 1; i < lines.length; ++i) {
-        var line = lines[i];
-        var m = line.match(/([1-9]{1}|10)([0-9])?\t(.+)/);
-        if(m === null) {
-            continue;
-        }
-
-        var rating = m[1];
-        var second = m[2];
-        if(second === undefined) {
-            // If second value is undefined the rating is < 10 (on criticker)
-            // and will be changed to 1 because you can't rate 0 on IMDb
-            rating = "1";
-        }
-
-        $("#filmList").append(rating + "," + m[3] + "\n");
-    }
-}
-
-function handleImport(evt) {
-    var files = evt.target.files;
-    var file = files[0];
-    var reader = new FileReader();
-    reader.onload = function(event){
-        var file = event.target.result;
-        var format = $("select[name=import]").val();
-        if(format === "none") {
-            alert("Select importer and try again.");
-            return;
-        }
-        else if(format === "imdb") {
-            processImdb(file);
-        }
-        else if(format === "rym") {
-            processRym(file);
-        }
-        else if(format === "criticker") {
-            processCriticker(file);
-        }
-    }
-
-    reader.readAsText(file);
-}
-
-var ListManager = {
-    regex: "^(.*)$",
-    processRegex: function(rx, cb) {
-        var filmTitle = rx[1];
-        cb(filmTitle);
+const parsers = {
+    imdb: line => {
+        const fields = line.split('","');
+        if (fields.length !== 16 || !fields[1].startsWith('tt') || !fields[8]) return null;
+        return { movie: fields[1], rating: fields[8] };
     },
-    handleSelection: function(imdbId, cb) {
-        cb();
+    rym: line => {
+        const fields = line.split('","');
+        if (fields.length < 6) return null;
+        return { movie: fields[1], rating: fields[4] };
+    },
+    criticker: line => { // if exported as .txt
+        const fields = line.match(/(\d+)\t(.+)/);
+        if (!fields || Number.isNaN(+fields[1])) return null;
+        return { movie: fields[2], rating: Math.ceil(+fields[1] / 10) || 1 };
+    },
+};
+
+const process = (file, parser) => {
+    for (const line of file.split('\n')) {
+        const m = parser(line);
+        if (!m) continue;
+        $('#film-list').append(`${m.rating},${m.movie}\n`);
     }
 };
 
-var RatingManager = {
+const handleImport = e => {
+    const reader = new FileReader();
+    reader.onload = event => {
+        const file = event.target.result;
+        const format = $('select[name=import]').val();
+        if (format === 'none') {
+            alert('Select importer and try again.');
+        } else if (parsers[format]) {
+            process(file, parsers[format]);
+        }
+    };
+
+    const [file] = e.target.files;
+    reader.readAsText(file);
+};
+
+const ListManager = {
+    regex: '^(.*)$',
+    processRegex: ([, filmTitle], cb) => cb(filmTitle),
+    handleSelection: (imdbId, cb) => cb(),
+};
+
+const RatingManager = {
     rating: 0,
-    regex: "^([1-9]{1}|10),(.*)$",
-    processRegex: function(rx, cb) {
-        RatingManager.rating = rx[1];
-        var filmTitle = rx[2];
+    regex: '^([1-9]|10),(.*)$',
+    processRegex: ([, rating, filmTitle], cb) => {
+        RatingManager.rating = rating;
         cb(filmTitle);
     },
-    handleSelection: function(imdbId, cb) {
-        console.log("RatingManager::handleSelection: Rating " + imdbId);
-        $.get("http://www.imdb.com/title/" + imdbId, function(data) {
-            var authHash = $(data).find("#star-rating-widget").data("auth");
-            var params = {tconst: imdbId, rating: RatingManager.rating,
-                            auth: authHash, tracking_tag: "list",
-                            pageId: imdbId, pageType: "title", subPageType: "main"};
-            $.post("http://www.imdb.com/ratings/_ajax/title", params, function(data) {
-                if(data.status !== 200) {
-                    alert("Rating failed. Status code " + data.status);
-                }
-                else {
+    handleSelection: (imdbId, cb) => {
+        console.log(`RatingManager::handleSelection: Rating ${imdbId}`);
+        $.get(`http://www.imdb.com/title/${imdbId}`, data => {
+            const authHash = $(data).find('#star-rating-widget').data('auth');
+            const params = {
+                tconst: imdbId,
+                rating: RatingManager.rating,
+                auth: authHash,
+                tracking_tag: 'list',
+                pageId: imdbId,
+                pageType: 'title',
+                subPageType: 'main',
+            };
+            $.post('http://www.imdb.com/ratings/_ajax/title', params, resp => {
+                if (resp.status !== 200) {
+                    alert(`Rating failed. Status code ${resp.status}`);
+                } else {
                     cb();
                 }
-            }, "json");
+            }, 'json');
         });
-    }
+    },
 };
 
-var App = {
+const App = {
     manager: ListManager,
     films: [],
     regexObj: null,
-    isEmpty: function() {
-        return App.films.length === 0;
-    },
-    next: function() {
-        return App.films.shift();
-    },
-    run: function() {
-        GM_addStyle("#imdbListTextEdit { margin: 0 5% 5% 5%; } \
-                     #imdbListTextEdit input[type=text], #imdbListTextEdit textarea { width: 100%; } \
-                     #imdbListTextEdit textarea { background-color: lightyellow; }");
-        var textEdit = '<div id="imdbListTextEdit" style="'
-                    + 'padding: 10px; border: 1px solid #e8e8e8">'
-                    + '<p><b>Import mode:</b><input type="radio" name="importmode" value="list" checked="checked">List</input>'
-                    + '<input type="radio" name="importmode" value="ratings">Ratings</input></p>'
-                    + '<textarea id="filmList" rows="7" cols="60" placeholder="Input titles or IMDb IDs and click Start"></textarea><br />'
-                    + '<input type="button" id="doList" value="Start" /> '
-                    + '<input type="button" id="skipFilm" value="Skip" /> '
-                    + '<input type="button" id="retryPost" value="Retry" /> '
-                    + '<span style="font-weight: bold">Remaining: <span id="filmsRemaining">0</span></span><br /><br />'
-                    + '<span style="font-weight: bold;">Current: <input type="text" id="filmCurrent" size="65" style="font-family: monospace" /></span><br />'
-                    + '<span style="font-weight: bold;">Regexp (matches only): <input type="text" value="" id="filmRegexp" size="65" style="font-family: monospace; margin-top: 4px; margin-left: 1px" /></span><br />'
-                    + '<p id="importform" style="display: none"><b>Import from:</b> <select name="import"><option value="none">Select</option><option value="imdb">IMDb</option>'
-                    + '<option value="rym">RateYourMusic</option><option value="criticker">Criticker</option></select><b> File: </b><input type="file" id="fileimport"></p>'
-                    + '</div>';
-        $("div.lister-search").after(textEdit);
+    isEmpty: () => App.films.length === 0,
+    next: () => App.films.shift(),
+    run: () => {
+        GM_addStyle(`
+            #imdb-list-text-edit {
+                margin: 0 5% 5% 5%;
+                padding: 10px;
+                border: 1px solid #e8e8e8;
+            }
 
-        $("#filmRegexp").val(App.manager.regex);
+            #imdb-list-text-edit input[type=text] {
+                width: 100%;
+                font-family: monospace;
+            }
 
-        $("#fileimport").on("change", handleImport);
+            #imdb-list-text-edit #film-regexp {
+                margin-top: 4px;
+                margin-left: 1px;
+            }
 
-        $("#imdbListTextEdit").on("change", "input[name=importmode]", function() {
-            var value = $(this).val();
-            if(value === "list") {
+            #imdb-list-text-edit textarea {
+                width: 100%;
+                background-color: lightyellow;
+            }
+
+            #imdb-list-text-edit span {
+                font-weight: bold;
+            }
+        `);
+        const textEdit = `
+            <div id="imdb-list-text-edit">
+                <p>
+                    <b>Import mode:</b>
+                    <input type="radio" name="importmode" value="list" checked="checked">List</input>
+                    <input type="radio" name="importmode" value="ratings">Ratings</input>
+                </p>
+                <textarea id="film-list" rows="7" cols="60" placeholder="Input titles or IMDb IDs and click Start"></textarea>
+                <br>
+                <input type="button" id="doList" value="Start">
+                <input type="button" id="skipFilm" value="Skip">
+                <input type="button" id="retryPost" value="Retry">
+                <span>Remaining: <span id="films-remaining">0</span></span>
+                <br><br>
+                <span>Current: <input type="text" id="film-current" size="65""></span>
+                <br>
+                <span>Regexp (matches only): <input type="text" id="film-regexp" size="65"></span>
+                <br>
+                <p id="importform" style="display: none">
+                    <b>Import from:</b>
+                    <select name="import">
+                        <option value="none">Select</option>
+                        <option value="imdb">IMDb</option>
+                        <option value="rym">RateYourMusic</option>
+                        <option value="criticker">Criticker</option>
+                    </select>
+                    <b>File:</b>
+                    <input type="file" id="fileimport">
+                </p>
+            </div>`;
+        $('div.lister-search').after(textEdit);
+
+        $('#film-regexp').val(App.manager.regex);
+
+        $('#fileimport').on('change', handleImport);
+
+        $('#imdb-list-text-edit').on('change', 'input[name=importmode]', function () {
+            const value = $(this).val();
+            if (value === 'list') {
                 App.manager = ListManager;
-                $("#importform").hide();
-            }
-            else {
+                $('#importform').hide();
+            } else {
                 App.manager = RatingManager;
-                $("#importform").show();
+                $('#importform').show();
             }
 
-            $("#filmRegexp").val(App.manager.regex);
+            $('#film-regexp').val(App.manager.regex);
         });
 
         // When start button is clicked
-        $("#imdbListTextEdit").on("click", "#doList", function(e) {
-            $regexBox = $("#filmRegexp");
-            if($regexBox.val()) {
+        $('#imdb-list-text-edit').on('click', '#doList', () => {
+            const $regexBox = $('#film-regexp');
+            if ($regexBox.val()) {
                 App.regexObj = RegExp($regexBox.val());
-            }
-            else {
+            } else {
                 App.regexObj = RegExp(App.manager.regex);
             }
 
             // Disable the text area and the button and the regexp box
             // as well as the import mode
-            $filmList = $("#filmList");
-            $filmList.attr("disabled", "disabled");
-            $regexBox.attr("disabled", "disabled");
-            $("#doList").attr("disabled", "disabled");
-            $("input[name=importmode]").attr("disabled", "disabled");
+            const $filmList = $('#film-list');
+            $filmList.attr('disabled', 'disabled');
+            $regexBox.attr('disabled', 'disabled');
+            $('#doList').attr('disabled', 'disabled');
+            $('input[name=importmode]').attr('disabled', 'disabled');
 
-            App.films = $filmList.val().split("\n");
+            App.films = $filmList.val().split('\n');
             App.handleNext();
         });
 
         // when skip button is clicked
-        $("#imdbListTextEdit").on("click", "#skipFilm", function(e) {
+        $('#imdb-list-text-edit').on('click', '#skipFilm', () => {
             App.handleNext();
         });
 
         // Sometimes the request fails forcing the user to skip an entry to continue
-        $("#imdbListTextEdit").on("click", "#retryPost", function(e) {
-            $("#add-to-list-search").trigger("keydown");
+        $('#imdb-list-text-edit').on('click', '#retryPost', () => {
+            $('#add-to-list-search').trigger('keydown');
         });
     },
-    reset: function() {
+    reset: () => {
         App.films = [];
         App.regexObj = null;
 
-        $("#filmList").removeAttr("disabled");
-        $("#filmRegexp").removeAttr("disabled"); // leave regex
-        $("#doList").removeAttr("disabled");
-        $("input[name=importmode]").removeAttr("disabled");
+        $('#film-list').removeAttr('disabled');
+        $('#film-regexp').removeAttr('disabled'); // leave regex
+        $('#doList').removeAttr('disabled');
+        $('input[name=importmode]').removeAttr('disabled');
 
-        $("#filmCurrent").val("");
+        $('#film-current').val('');
 
-        $("#add-to-list-search", "div.add").val("");
-        //$("div.results", "div.add").html("");
+        $('#add-to-list-search', 'div.add').val('');
+        // $("div.results", "div.add").html("");
     },
-    search: function(filmTitle) {
+    search: filmTitle => {
         // remove unnecessary whitespace
         filmTitle = $.trim(filmTitle);
 
         // set current text to what we're searching
-        $("#filmCurrent").val(filmTitle);
+        $('#film-current').val(filmTitle);
 
         // remove the first title from the text box and set the remaining number
-        $filmList = $("#filmList");
-        var newList = $filmList.val().split("\n");
-        $("#filmsRemaining").text(newList.length-1);
-        $filmList.val(newList.slice(1).join("\n"));
+        const $filmList = $('#film-list');
+        const newList = $filmList.val().split('\n');
+        $('#films-remaining').text(newList.length - 1);
+        $filmList.val(newList.slice(1).join('\n'));
 
         // Run regex if it matches and let the manager process the result
-        var result = App.regexObj.exec(filmTitle);
-        if(result !== null) {
-            App.manager.processRegex(result, function(filmTitle) {
+        const result = App.regexObj.exec(filmTitle);
+        if (result !== null) {
+            App.manager.processRegex(result, filmTitle2 => {
                 // Set imdb search input field to film title
-                $("#add-to-list-search").val(filmTitle);
+                $('#add-to-list-search').val(filmTitle2);
 
                 // And perform search
-                $("#add-to-list-search").trigger("keydown");
+                $('#add-to-list-search').trigger('keydown');
             });
-        }
-        else {
+        } else {
             App.handleNext();
         }
     },
-    handleNext: function() {
-        if(!App.isEmpty()) {
+    handleNext: () => {
+        if (!App.isEmpty()) {
             // if there's more items, search next...
             App.search(App.next());
-        }
-        else {
+        } else {
             // if last film
             App.reset();
         }
-    }
+    },
 };
 
 $(document).ready(App.run);
 
 // When a search result item is clicked by user or script
-$("#add-to-list-search-results").on("click", "a", function(e) {
-    App.manager.handleSelection($(this).attr("id"), function() {
+$('#add-to-list-search-results').on('click', 'a', function () {
+    App.manager.handleSelection($(this).attr('id'), () => {
         // Some delay is needed
-        setTimeout(function() {
-            App.handleNext();
-        }, REQUEST_DELAY);
+        setTimeout(() => App.handleNext(), REQUEST_DELAY);
     });
 });
 
 // Monitors for changes to the search result box
 // If it recognizes an IMDBb URL/ID, it's clicked automatically
 // since there's only one result
-var clickId = null;
-$("#add-to-list-search-results").bind("DOMNodeInserted", function(e) {
-    if($("#filmCurrent").val().match(/([CHMNTchmnt]{2}[0-9]{7})/) !== null && $("a", "#add-to-list-search-results").length) {
+let clickId = null;
+$('#add-to-list-search-results').bind('DOMNodeInserted', () => {
+    if ($('#film-current').val().match(/([CHMNTchmnt]{2}[0-9]{7})/) !== null &&
+        $('a', '#add-to-list-search-results').length) {
         // Some delay is needed for all results to appear
-        clickId = setTimeout(function() {
-            $("a", "#add-to-list-search-results").first()[0].click();
+        clickId = setTimeout(() => {
+            $('a', '#add-to-list-search-results').first()[0].click();
             clearTimeout(clickId);
         }, REQUEST_DELAY);
     }
